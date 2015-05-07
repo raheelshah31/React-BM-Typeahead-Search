22caf56c657a15c8a883e8299acb70ee6b7979a6
Raheel Shah <raheel.shah31@gmail.com>
2015-05-07 10:06:58 +0530

    React BM Type Ahead
    Create a react component that lists Now Showing movies (refer to
    BookMyShow’s website) along with a search + typeahead input box that
    lets a User filter a Movie, Cinema or both. Cinemas here are just
    category filters for the Movie titles. For example if I type a Cinema
    named CineMax, then I should see all the movies mapped to CineMax.
    Another scenario is if I type “Avengers CineMax”, I should see Avengers
    but it’s state should include CineMax as well. To summarize, you will
    just list the movie names and the associated Cinemas are meta
    information tags.
    
    Assume the Movie/Cinema data is available in a JS Object which you can
    structure the way you like.
    
    You’re free to add any functionality that you think might add value to
    the search. The aim is for Users to find what they’re looking for easily
    & fast.

---
+++ README.md
@@ -0,0 +1,14 @@
+Dev Test #5
+You will be marked primarily on your problem solving approach but cleaner, well indented and beautiful code will be appreciated. Please host your work on Github and share the link with us. 
+Create a react component that lists Now Showing movies (refer to BookMyShow’s website) along with a search + typeahead input box that lets a User filter a Movie, Cinema or both. Cinemas here are just category filters for the Movie titles. For example if I type a Cinema named CineMax, then I should see all the movies mapped to CineMax. Another scenario is if I type “Avengers CineMax”, I should see Avengers but it’s state should include CineMax as well. To summarize, you will just list the movie names and the associated Cinemas are meta information tags.
+
+Assume the Movie/Cinema data is available in a JS Object which you can structure the way you like.
+
+You’re free to add any functionality that you think might add value to the search. The aim is for Users to find what they’re looking for easily & fast.
+
+Implementing this using a Flux pattern will be appreciated; but not necessary.
+
+You can get back to us if you have any doubts. 
+
+After it’s done
+Push all of this code onto github. Please add adequate comments to explain your approach/logic.
---
+++ component/search.jsx
@@ -0,0 +1,244 @@
+/* Search
+==================================== */
+// Search Main
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+var SearchArea = React.createClass({
+
+    // Gets initial state, duh
+    getInitialState: function() {
+        return {
+            data: {
+                items: []
+            }
+        };
+    },
+
+    componentDidMount: function(url, _this, data) {
+
+        $('#search').focus();
+        
+
+
+    },
+
+    getResults: function(e) {
+
+        var _this = this;
+
+        var date = new Date();
+        var q = $('#search').val();
+        var api_url = this.props.api_url;
+
+        switch (e.which) {
+
+            case 38: // up
+                var currentId = $('#search-results .result.active').attr('data-id');
+                var prevId = parseInt(currentId, 10) - 1;
+                $('#search-results .result').removeClass('active');
+                if (!$('#search-results #result-' + prevId).length) return false;
+                $('#search-results #result-' + prevId).addClass('active');
+                break;
+
+            case 40: // down
+                if (!$('#search-results .result.active').length) {
+
+                    $('#search-results .result').first().addClass('active');
+
+                } else {
+
+                    var currentId = $('#search-results .result.active').attr('data-id');
+                    var nextId = parseInt(currentId, 10) + 1;
+                    if (!$('#search-results #result-' + nextId).length) return false;
+                    $('#search-results .result').removeClass('active');
+                    $('#search-results #result-' + nextId).addClass('active');
+
+                }
+                break;
+
+            case 13: // enter
+                var link = $('#search-results .result.active').attr('href');
+                window.open(link);
+                break;
+
+            default:
+
+                var q = $('#search').val();
+                var url = api_url + '?q=' + q + '&time=' + date.getTime(); // for yo cache
+
+                $.getJSON(url, function(data) {
+
+                    var items = data.items;
+                    var newItems = [];
+                    var q = $('#search').val();
+                    var qTemp=$('#search').val();
+                    q = q.split(" ") //if user types two or more keywords
+                        // Do your searching here
+                    q.forEach(function(q, a) { //itreate and refine search results for each keyword hit
+                        if (q != "") { //No need to iterate if keyword is blank
+                            items.forEach(function(item, i) {
+                                var qLower = q.toLowerCase();
+                                var titleLower = item.name.toLowerCase();
+                                console.log(item.name);
+                                var cinemas = item.cinemas;
+                                var formattedTitle = highlight(item.name, q);
+                                // var formattedContent = highlight(item.content, q);
+                                var cinema;
+                                var hitFlag = false;
+                                var newHlCinemaArray = []; //Store Cinema hits highligted
+
+                                // item.formattedContent = formattedContent;
+                                item.name = formattedTitle;
+
+                                cinemas.forEach(function(item, i) {
+                                    console.log(item + " ---" + i);
+                                    cinema = item.toLowerCase();
+                                    if (cinema.indexOf(qLower) != -1) {
+                                        hitFlag = true;
+                                        newHlCinemaArray.push(highlight(item, q)); //unique hit entries
+                                    } 
+                                });
+                                if(hitFlag){
+                                     item.cinemas = newHlCinemaArray;
+                                }else{
+
+                                }
+                               
+                                console.log("Naya" + item);
+                                // Add custom search criteria here
+                                if (titleLower.indexOf(qLower) !== -1 || hitFlag) {
+                                    newItems.push(item);
+                                }
+
+
+
+
+                            });
+                        }
+                    });
+
+                    console.log(newItems.length)
+                    if (!newItems.length && qTemp=="") { //If user input is blank show all the movies
+
+                        newItems=items;
+                    } else if(qTemp!=""){
+                     
+                    }
+
+                    if (_this.isMounted()) {
+                        _this.setState({
+                            data: {
+                                items: newItems
+                            }
+                        });
+                    }
+
+                });
+
+                return; // exit this handler for other keys
+        }
+    },
+
+    render: function() {
+
+        return (
+            <div className="row">
+            
+     <div className="col-lg-12 " >
+      <input id="search"  className="form-control" onfocus={this.getResults}   onKeyDown={this.getResults}  type="text" placeholder={this.props.placeholder} />
+        <SearchList items={this.state.data.items} />
+   
+    </div>
+    
+    </div>
+
+
+        );
+    }
+});
+
+// List Component
+var SearchList = React.createClass({
+
+    render: function() {
+
+        var rows;
+        var items = this.props.items;
+
+        if (items) {
+
+            rows = items.map(function(item, i) {
+
+                return (
+                    <ListItem item={item} id={i}/>
+                );
+
+            }.bind(this));
+
+        }
+
+        return (
+            <ul id="search-results" >
+          {rows}
+        </ul>
+        );
+
+    }
+
+});
+
+// List Item
+var ListItem = React.createClass({
+
+    render: function() {
+
+        var item = this.props.item;
+        var id = this.props.id;
+
+        return (
+
+            <div class="row" key={item}>
+        <div className="col-sm-6 col-md-3">
+          <div className="thumbnail">
+            <img className="movImg" src={item.img}  alt="..."></img>
+              <div className="caption">
+                <h3 dangerouslySetInnerHTML={{__html: item.name}}></h3>
+                <TagCinema items={item.cinemas}/>
+              </div>
+            </div>
+          </div>
+       </div>
+      
+        );
+        return(
+                <ReactCSSTransitionGroup transitionName="example">
+               {item}
+                </ReactCSSTransitionGroup>
+
+          )
+    }
+});
+
+React.render(<SearchArea api_url="api/data.json" placeholder="Search movies/cinema"  />, document.getElementById('search-area'));
+
+
+var TagCinema = React.createClass({
+    render: function() {
+        return (
+            <div>
+        {this.props.items.map(function(answer, i) {
+          return (
+             <button  className="btn btn-default btn-sm"  bsStyle='info' dangerouslySetInnerHTML={{__html: answer}}></button>
+          );
+        }, this)}
+      </div>
+        );
+    }
+})
+
+function preg_quote(str) {
+    return (str + '').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+}
+
+function highlight(data, search) {
+    return data.replace(new RegExp("(" + preg_quote(search) + ")", 'gi'), "<b>$1</b>");
+}
